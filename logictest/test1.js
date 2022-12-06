09f4e4d8624f490a7717e0f3da4fd5993ce0b2bc
Yoga Permana <yopernurr.s@gmail.com>
2022-12-06 15:21:52 +0700

    initial commit

---
+++ logictest/test1.js
@@ -0,0 +1,9 @@
+let words = "beever";
+
+// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut : 
+// b
+// be
+// bee
+// beev
+// beeve
+// beever
---
+++ src/components/PersonalQuoteView.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function PersonalQuoteView() {
+
+  return (
+    <section style={{ textAlign: "center", marginTop: "60px" }}>
+      <hr style={{ maxWidth: "30%" }} />
+      <h2>My Quotes</h2>
+      <form >
+        <input
+        />
+        <button
+          type={`submit`}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
