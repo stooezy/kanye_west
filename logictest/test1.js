b87f8a4c6632ad3b04848a44b2a99ecceebb12a9
Yoga Permana <yopernurr.s@gmail.com>
2022-12-06 15:50:40 +0700

    submit: test1.js

--- logictest/test1.js
+++ logictest/test1.js
@@ -7,3 +7,18 @@ let words = "beever";
 // beev
 // beeve
 // beever
+
+/**
+ * Answer
+ * 
+ * Yoga Permana
+ */
+const wordTriangle = (sentence) => {
+  for (let i = 1; i <= sentence.length; i++) {
+    console.log(sentence.substring(0, i))
+  }
+}
+
+
+// debug
+wordTriangle(words)
